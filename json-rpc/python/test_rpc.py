e7fa7d895874e388822311325d9fed7b99cf331c
Kaiwen Zhang <kevin900718@gmail.com>
2014-11-18 19:54:36 -0800

    updated

--- json-rpc/python/test_rpc.py
+++ json-rpc/python/test_rpc.py
@@ -2,7 +2,12 @@ import socket
 from jsonrpc import loads,dumps
 
 json = {"method":"getTickData","params":["AAPL"],"id":"req-01","jsonrpc":"2.0"}
+req = dumps(json)
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(("localhost",8080))
 
-sock.send(dumps(json))
+sock.send(req)
+## Not receiving response from Java server
+# resp = sock.recv(1024)
+# print resp
+sock.close()
