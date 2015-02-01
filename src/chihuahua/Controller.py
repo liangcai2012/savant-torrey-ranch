5b9390ff91549a6bbed645a733edb3941685866d
Kaiwen Zhang <kevin900718@gmail.com>
2015-02-01 15:00:03 -0800

    trigger and controller connection

--- src/chihuahua/Controller.py
+++ src/chihuahua/Controller.py
@@ -1,8 +1,9 @@
-class Controller:
+import sys
+import rpyc
+from rpyc.utils.server import ThreadedServer
 
-    def __init__(self,lstStrat):
-	self.lstStrat = set(lstStrat)
-   
+class Controller(rpyc.Service):
+
     def invokeWorker(self):
         for strat in self.lstStrat:
             try:
@@ -12,6 +13,14 @@ class Controller:
             except:
                 print "Strategy class " + strat + " doesn't exist!"
 
+    def exposed_request(self,request):
+        print request
+
+    def exposed_exit(self):
+        "Exiting Controller"
+        sys.exit(0)
+
 if __name__ == "__main__":
-    c = Controller(["Simple","Complex"])
-    c.invokeWorker()
+    server = ThreadedServer(Controller,port=8088)
+    print "Starting Controller"
+    server.start()
