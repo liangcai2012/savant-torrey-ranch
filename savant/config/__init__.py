4d81778edc55eca083b7233f23a4bb775c194693
Kaiwen Zhang <kevin900718@gmail.com>
2015-06-17 00:28:32 -0700

    changed settings file to .py file to allow relative path;allow customizable setting

--- savant/config/__init__.py
+++ savant/config/__init__.py
@@ -1,5 +1,6 @@
-import os, os.path
-from ConfigParser import ConfigParser, NoSectionError
+import os
+import imp
+from savant.util import import_string
 
 
 class AttributeDictMixin(object):
@@ -20,33 +21,53 @@ class AttributeDictMixin(object):
 
 class SavantConfig(dict, AttributeDictMixin):
 
-    def __init__(self, config_file):
+    def __init__(self):
         dict.__init__(self, {})
-        self.config_file = config_file
-        self.parser = ConfigParser()
-        self.import_config()
 
-    def import_config(self):
+    def from_object(self, obj):
+        if isinstance(obj, str):
+            obj = import_string(obj)
+        for key in dir(obj):
+            if key.isupper():
+                self[key] = getattr(obj, key)
+
+    def from_envvar(self, varname, silent=False):
+        var = os.environ.get(varname)
+        if not var:
+            if silent:
+                return False
+            raise RuntimeError("The envrionment variable %s is not set" % varname)
+        return self.from_pyfile(var, silent=silent)
+
+    def from_pyfile(self, filename, silent=False):
+        d = imp.new_module("config")
+        d.__file__ = filename
         try:
-            self.parser.readfp(open(self.config_file))
-        except IOError:
+            with open(filename) as f:
+                exec(compile(f.read(), filename, "exec"), d.__dict__)
+        except IOError as e:
+            if silent and e.errno in (errno.ENDENT, errno.EISDIR):
+                return False
+            e.strerror = "Unable to load configuration file (%s)" % e.strerror
             raise
-
-        try:
-            configs = self.parser.items("SAVANT")
-            for key, value in configs:
-                self[key.upper()] = value.strip("\'")
-        except NoSectionError:
-            raise NoSectionError("No Section \'SAVANT\' in configuration file")
+        self.from_object(d)
+        return True
 
     def __repr__(self):
         return "<%s %s>" % (self.__class__.__name__, dict.__repr__(self))
 
-
-default_settings_file = os.path.join(os.path.split(__file__)[0],"default_settings.ini")
-settings = SavantConfig(default_settings_file)
-
+# The env var pre-defined
+ENVIRONMENT_VARIABLE = "SAVANT_SETTINGS"
+# Set up config
+settings = SavantConfig()
+# Read default settings
+settings.from_object("savant.config.default_settings")
+# Read customized settings if exists
+#settings.from_envvar(ENVIRONMENT_VARIABLE)
+print settings
+"""
 alembic_settings_file = os.path.join(os.path.dirname(__file__), "alembic.ini")
 config = ConfigParser()
 config.read(alembic_settings_file)
 db_settings = config._sections
+"""
