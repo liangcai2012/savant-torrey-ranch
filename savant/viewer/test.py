7744959431584066351556de5d9859ce088ff028
wholmes123 <wangchuan827@gmail.com>
2015-07-15 03:14:27 -0700

    Update test.py
    update test cases

--- savant/viewer/test.py
+++ savant/viewer/test.py
@@ -4,8 +4,12 @@ import time
 
 
 def test():
-    command_add1=dumps({'cmd':'add', 'symbol':'QQQ', 'type':'r','interval':'1s','pos':0,'price':'h','movingave':['1m','5m'],'volume':'y'})
-    command_add2=dumps({'cmd':'add', 'symbol':'QQQ', 'type':'r','interval':'1s','pos':0,'price':'l','movingave':['1m','5m'],'volume':'y'})
+    command_add11=dumps({'cmd':'add', 'symbol':'QQQ', 'type':'r','interval':'1s','pos':0,'price':'h','movingave':['1m','5m'],'volume':'y'})
+    command_add12=dumps({'cmd':'add', 'symbol':'QQQ', 'type':'r','interval':'1s','pos':0,'price':'l','movingave':['1m','5m'],'volume':'y'})
+    command_add13=dumps({'cmd':'add', 'symbol':'QQQ', 'type':'r','interval':'1s','pos':0,'price':'o','movingave':['1m','5m'],'volume':'y'})
+    command_add14=dumps({'cmd':'add', 'symbol':'QQQ', 'type':'r','interval':'1s','pos':0,'price':'c','movingave':['1m','5m'],'volume':'y'})
+    
+    
     command_add3=dumps({'cmd':'add', 'symbol':'QQQ', 'type':'r','interval':'5s','pos':0,'price':'l','movingave':['1m','5m'],'volume':'y'})
     command_add4=dumps({'cmd':'add', 'symbol':'QQQ', 'type':'h','interval':'1s','pos':0,'price':'l','movingave':['1m','5m'],'volume':'y','start':'2015-01-01 09:00:00','end':'2015-01-01 15:00:00'})
     
@@ -14,12 +18,18 @@ def test():
     command_del=dumps({'cmd':'del', 'id':0,'pos':0})
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)            
     client.connect(("localhost", 8091))
-    client.send(command_add1)
-    print 'send add1'
-    time.sleep(10)
-    client.send(command_add2)
-    print 'send add2'
-    time.sleep(10)
+    client.send(command_add11)
+    print 'send add11'
+    time.sleep(5)
+    client.send(command_add12)
+    print 'send add12'
+    time.sleep(5)
+    client.send(command_add13)
+    print 'send add13'
+    time.sleep(5)
+    client.send(command_add14)
+    print 'send add14'
+    time.sleep(5)
 #     client.send(command_add3)
 #     print 'send add3'
 #     time.sleep(1)
