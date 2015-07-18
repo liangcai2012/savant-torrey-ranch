16e4190b144e2f9622748ecd04dda03fd38baef8
wholmes123 <wangchuan827@gmail.com>
2015-07-19 06:53:03 +0800

    Update IPO_FilterByOpen.py

--- savant/strategy/strategies/IPO_FilterByOpen.py
+++ savant/strategy/strategies/IPO_FilterByOpen.py
@@ -1,14 +1,14 @@
 import sqlite3
 
 def filter_openPrice(x):
-    conn = sqlite3.connect('savant.db')
+    conn = sqlite3.connect('../../../out/savant.db')
     c = conn.cursor()
     times=x
     str1="SELECT symbol FROM company WHERE id in (SELECT company_id FROM historical_ipo WHERE (first_opening_price> (price*%s) AND price!=0 ) );" %times
+    c.execute(str1) 
     symbollist=c.fetchall()
     print symbollist 
     
 if __name__ == "__main__":
     filter_openPrice(20)
-
 
