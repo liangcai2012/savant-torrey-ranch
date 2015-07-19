466a8f248c665adb668b5c2f1d63026cd3e85e9a
wholmes123 <wangchuan827@gmail.com>
2015-07-19 17:44:59 +0800

    Create IPO_RateFinder.py
    input symbol, output it's rate

---
+++ savant/strategy/strategies/IPO_RateFinder.py
@@ -0,0 +1,32 @@
+import pandas as pd
+# import sqlite3
+
+def rate_finder(sym):
+    header_row=['date','name','symbol','underwriter','a','b','c','d','e','f','rate','meet']
+    data=pd.read_csv('iposcoop.csv' , header=None, names=header_row)
+
+    df=pd.DataFrame(data)
+    # print data
+    temp= pd.DataFrame(df.loc[df['symbol']==(sym)])
+    temp.date=pd.to_datetime(temp.date)
+
+    result=temp.sort('date')[-1:]['rate'] #sort by date->get last raw->get rate
+#   print  type(result)
+    print "The rate of '%s' is:" %sym, result.iloc[0]  #get item from pandas series   
+    
+    
+if __name__ == "__main__":
+    rate_finder('BOX')
+
+
+
+
+
+
+
+
+
+
+
+
+
