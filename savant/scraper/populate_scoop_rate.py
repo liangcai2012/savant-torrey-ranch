30c2268966ecce78f724857d7266015e4ed2d6be
Liang Cai <liangcai@liangs-mbp.attlocal.net>
2015-08-15 15:35:51 -0700

    adding populate_scoop_rate

--- savant/scraper/populate_scoop_rate.py
+++ savant/scraper/populate_scoop_rate.py
@@ -1,22 +1,44 @@
 import pandas as pd
+from savant.config import settings
+from savant.db.models import * 
+from savant.db import session
 # import sqlite3
 
 def rate_finder(sym):
     header_row=['date','name','symbol','underwriter','a','b','c','d','e','f','rate','meet']
-    data=pd.read_csv('../../../out/iposcoop.csv' , header=None, names=header_row)
+    data=pd.read_csv(settings.DATA_HOME+'/iposcoop.csv' , header=None, names=header_row)
 
     df=pd.DataFrame(data)
     # print data
     temp= pd.DataFrame(df.loc[df['symbol']==(sym)])
+    if len(temp) == 0:
+        return None
     temp.date=pd.to_datetime(temp.date)
 
     result=temp.sort('date')[-1:]['rate'] #sort by date->get last raw->get rate
-#   print  type(result)
-    print "The rate of '%s' is:" %sym, result.iloc[0]  #get item from pandas series   
+#   print  type(result) return result.iloc[0]
+    #print "The rate of '%s' is:" %sym, result.iloc[0]  #get item from pandas series   
+    return result.iloc[0]
     
-    
+
+def update_scoop_rate():
+    ipos  = session.query(Company, HistoricalIPO).filter(Company.id == HistoricalIPO.company_id).all() 
+    for ipo in ipos:
+        sym = ipo.Company.symbol
+        rate = rate_finder(sym)
+        if rate == None:
+            continue
+        hi = HistoricalIPO.query.filter(HistoricalIPO.company_id == ipo.Company.id).first()
+        if hi == None:
+            continue #should not happen
+        hi.scoop_rating = rate
+    session.commit()    
+
+
+
 if __name__ == "__main__":
-    rate_finder('BOX')
+   update_scoop_rate()
+   # rate_finder('ABCW')
 
 
 
