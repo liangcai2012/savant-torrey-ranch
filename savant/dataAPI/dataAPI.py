f4ea85f3d4a5ca8847ac3d4f362394154b196839
Liang Cai <liangcai@liangs-mbp.attlocal.net>
2015-08-15 14:00:35 -0700

    minor

--- savant/dataAPI/dataAPI.py
+++ savant/dataAPI/dataAPI.py
@@ -7,21 +7,21 @@ class DataError(RuntimeError):
       self.args = args
 
 class DataAPI:
-	def __init__(self, clientName, serverIP= None, port= None,parameters= None):
-		self.sc = streamer.getStreamerCaller()
-		self.client = clientName 
-		if severIP != None:
-			self.realtime = True
-		self.realtime = Fales
+    def __init__(self, clientName, serverIP= None, port= None,parameters= None):
+        self.sc = streamer.getStreamerCaller()
+        self.client = clientName 
+        if severIP != None:
+            self.realtime = True
+        self.realtime = Fales
 
 
 
-	def subscribeRealtime(self, symList):
-		if self.realtime:
-			jret = json.loads(self.sc.subscribe(self.client, symList))
-			if jret["response"]["errcode"] == 0:
-				return 0
-		return -1
+    def subscribeRealtime(self, symList):
+        if self.realtime:
+            jret = json.loads(self.sc.subscribe(self.client, symList))
+            if jret["response"]["errcode"] == 0:
+                return 0
+        return -1
 
 
     # subscrib a list of tuples [{sym, time}] in this instance for obtaining history.
@@ -29,28 +29,28 @@ class DataAPI:
     # raise an error if serverIp and port are specified in __init__
     # read multiple history data in one instance is not common. In most case the list 
     # should contain only one sym-time tuple. 
-	def subscribeHistory(self, symPeriodList):
-		pass
+    def subscribeHistory(self, symPeriodList):
+        pass
 
 
-	# unsubscribe part of symbols previous subscribed 
-	def unsubscribeRealtime(self):
-		if self.realtime:
-			jret = json.loads(self.sc.unsubscribe(self.client))
-			if jret["response"]["errcode"] == 0:
-				return 0
-		return -1
+    # unsubscribe part of symbols previous subscribed 
+    def unsubscribeRealtime(self):
+        if self.realtime:
+            jret = json.loads(self.sc.unsubscribe(self.client))
+            if jret["response"]["errcode"] == 0:
+                return 0
+        return -1
 
     # unsubscribe part of the symbol-time tuples previous subscribed. For history data, this api has no real effect
-	def unsubscribeHistory(self, symPeriodList):
-		pass
+    def unsubscribeHistory(self, symPeriodList):
+        pass
 
 
-	# update data with given interval
+    # update data with given interval
     # return a list of data for all subscribed symbol or symbol-time tuples. The returned data format can be seen in 
     def update(self, interval, bar_mask, ma_mask):
-		if self.realtime:
-			return json.loads(self.sc.unsubscribe(self.client))
+        if self.realtime:
+            return json.loads(self.sc.unsubscribe(self.client))
 
 
 #    def setReponseMsg(self, client, symbol, bar, ma, delay, timestamp, interval):
